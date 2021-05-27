a72f94b4cdb4cca2ccf824de5b87f19304e83882
awwadsaeed <saeedawwad450@gmail.com>
2021-05-27 18:26:12 +0300

    temporary memory

--- movies.js
+++ movies.js
@@ -17,22 +17,29 @@ class Movies {
     }
 }
 
+let arr={}
 function movieHandler(req, res) {
     let city = req.query.searchQuery;
     let url = `https://api.themoviedb.org/3/search/movie?api_key=${moviekey}&query=${city}`;
     // let url = 'https://api.themoviedb.org/3/search/movie?api_key=3fcd0d73c8ceb0f12a3b8581e0675f15&query=paris';
-    axios.get(url).then(result => {
-        let movieArr = result.data.results.map(element => {
-            return new Movies(element);
+    if(arr[city]!==undefined){
+        console.log('data from array');
+        res.send(arr[city]);
+    }else{
+        console.log('data from API')
+        axios.get(url).then(result => {
+            let movieArr = result.data.results.map(element => {
+                return new Movies(element);
+            })
+            arr[city]=movieArr;
+            res.send(movieArr);
         })
-        res.send(movieArr);
-        // console.log(movieArr);
-    })
-        .catch(error => {
-            console.log(error.response.status);
-            let e=error.response.status;
-            res.status(e).send(`data not found.${error}`);
-        })
+            .catch(error => {
+                console.log(error.response.status);
+                let e=error.response.status;
+                res.status(e).send(`data not found.${error}`);
+            })
+    }
 }
 
 module.exports = movieHandler;
